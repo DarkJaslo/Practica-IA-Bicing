2b03d4ec350a5ab8efd7004cec6d22b7eba878e8
Jon <jon.campillo@gmail.com>
2023-10-07 11:20:29 +0200

    hecho como en el ejemplo (ProbIA5)

--- practica/PracHeuristicFunction.java
+++ practica/PracHeuristicFunction.java
@@ -5,10 +5,6 @@ import aima.search.framework.HeuristicFunction;
 public class PracHeuristicFunction implements HeuristicFunction {
 
     public double getHeuristicValue(Object n){
-        //codigo
-
-        //valor de retorno cualquiera para quitar mensajes de error
-        return 0.0;
+        return ((PracBoard)n).heuristicFunction();
     }
-    
 }
