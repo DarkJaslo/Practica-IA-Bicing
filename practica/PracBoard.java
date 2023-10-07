e4a8a303b7c1dd6ed97829b5e2b1b35bc8dc98fc
Jon <jon.campillo@gmail.com>
2023-10-07 12:07:07 +0200

    add comentarios/sugerencias para pensar en los estados

--- practica/PracBoard.java
+++ practica/PracBoard.java
@@ -21,6 +21,10 @@ public class PracBoard {
      * Ejemplo: eA -20, eB +5, eC +15
      */
     private int [][] viajes; 
+    /*
+     * Parece útil
+     */
+    private int furgEnUso;
 
     /*
      * Constructora
@@ -28,5 +32,57 @@ public class PracBoard {
     public PracBoard(Estaciones estaciones, int maxFurgonetas){
         this.estaciones = estaciones;
         this.maxFurgonetas = maxFurgonetas;
+        this.furgEnUso = 0;
+    }
+
+    /* Operadores */
+
+    /*  
+     * Cambiar origen de una f
+     * Cambiar destino 1 de una f
+     * Cambiar destino 2 de una f
+     * Swapear destino 1 y destino 2 de una f
+     * 
+     * Swapear destino 1,2 de f1 con destino 1,2 de f2
+     * Swapear origen de f1 con origen de f2   
+     *
+     *  
+     * Cosas que seguro que optimizan:
+     * 
+     * Quitar furgonetas cuando sobran (redistribuir y borrar)
+     * Añadir furgonetas cuando faltan (añadir y redistribuir)
+     * 
+     * Borrar una furgoneta siempre que se pueda
+     * 
+     * 
+     * Qué hacer con los números? Siempre el máximo? 
+     *      Llevarte todas las que te puedas llevar
+     *      Dejar todas las que puedas? y la siguiente parada?
+     * 
+     * 
+     * En hill climbing vale la pena intentar redistribuir más?
+     * El coste es (nb+9)/10! 39/10 es 3, maximizar siempre el segundo dígito es bueno, llevar 20 eq 29
+    */
+
+
+
+
+    /*
+     * Función heurística
+     */
+    public double heuristicFunction(){
+        return 0.0;
+    }
+
+
+
+    /* Getters */
+    
+    public int[] getOcupacion(){
+        return ocupacion;
+    }
+
+    public int[][] getViajes(){
+        return viajes;
     }
 }
