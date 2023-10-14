f15ba66def7046bb980e4222c7c68213f4878eb3
Jon <jon.campillo@gmail.com>
2023-10-14 13:47:59 +0200

    Genera sucesores

--- practica/PracSuccessorFunction.java
+++ practica/PracSuccessorFunction.java
@@ -8,11 +8,84 @@ import java.util.List;
 public class PracSuccessorFunction implements SuccessorFunction {
     
     public List getSuccessors(Object state){
-        ArrayList retval = new ArrayList();
+
+        System.out.println("Generando sucesores");
+
+        ArrayList<Successor> retval = new ArrayList<Successor>();
+        PracBoard board = (PracBoard)state;
 
         /*
          * Añade todos los estados posibles a la lista
          */
+
+        /*
+         * Añade sucesores de changeEst
+         */
+        for(int i = 0; i < board.getFurgonetasEnUso(); ++i)
+        {
+            for(int j = 0; j < board.getEstaciones().size(); ++j)
+            {
+                if(board.canChangeEst(i, board.origen(), j)){
+                    PracBoard auxBoard = PracBoard.copyOf(board);
+                    auxBoard.changeEst(i, board.origen(), j);
+                    retval.add(new Successor("changeEst  f:" + i + ", st:ORIGEN, to:" + j, auxBoard));
+                }
+                if(board.canChangeEst(i, board.destino1(), j)){
+                    PracBoard auxBoard = PracBoard.copyOf(board);
+                    auxBoard.changeEst(i, board.destino1(), j);
+                    retval.add(new Successor("changeEst  f:" + i + ", st:DEST1, to:" + j, auxBoard));
+                }
+                if(board.canChangeEst(i, board.destino2(), j)){
+                    PracBoard auxBoard = PracBoard.copyOf(board);
+                    auxBoard.changeEst(i, board.destino2(), j);
+                    retval.add(new Successor("changeEst  f:" + i + ", st:DEST2, to:" + j, auxBoard));
+                }
+            }
+        }
+
+        /*
+         * Añade sucesores de swapEst
+         */
+        for(int f1 = 0; f1 < board.getFurgonetasEnUso(); ++f1)
+        {
+            for(int f2 = 0; f2 < board.getFurgonetasEnUso(); ++f2)
+            {
+                int ests[] = {board.origen(),board.origen(),
+                              board.destino1(),board.destino1(),
+                              board.destino1(),board.destino2(),
+                              board.destino2(),board.destino1(),
+                              board.destino2(),board.destino2()};
+
+                for(int i = 0; i < 10; i+=2)
+                {
+                    if(board.canSwapEst(f1, f2, ests[i], ests[i+1]))
+                    {
+                        PracBoard auxBoard = PracBoard.copyOf(board);
+                        auxBoard.swapEst(f1, f2, ests[i], ests[i+1]);
+                        retval.add(new Successor("swapEst  f1:" + f1 + ", f2:" + f2 + ", st1:" + board.getNombreEstacion(ests[i]) + ", st2:" + board.getNombreEstacion(ests[i+1]), auxBoard));
+                    }
+                }
+            }
+        }
+
+        /*
+         * Añade sucesores de addVan
+         */
+        for(int o = 0; o < board.getEstaciones().size(); ++o)
+        {
+            for(int d1 = 0; d1 < board.getEstaciones().size(); ++d1)
+            {
+                for(int d2 = 0; d2 < board.getEstaciones().size(); ++d2)
+                {
+                    if(board.canAddVan(o, d1, d2))
+                    {
+                        PracBoard auxBoard = PracBoard.copyOf(board);
+                        auxBoard.addVan(o, d1, d2);
+                        retval.add(new Successor("addVan o:" + o + ", d1:" + d1 + ", d2:" + d2, auxBoard));
+                    }
+                }
+            }
+        }
 
         return retval;
     }
