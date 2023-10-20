5d2ace829b4b652328e962fcc1549155674b196e
Jon <jon.campillo@gmail.com>
2023-10-20 10:10:36 +0200

    Ejecuta ya funciona en Windows

--- ejecuta.bat
+++ ejecuta.bat
@@ -1,6 +1,7 @@
+@echo off
 if "%~1"=="" (
-    echo Usage: ejecuta.bat <MainClassName>
-    echo Especifica que clase quieres ejecutar como main. Ejemplo: ejecuta.bat Tester1
+    echo "Usage: ejecuta.bat <MainClassName>"
+    echo "Especifica que clase quieres ejecutar como main. Ejemplo: ejecuta.bat Tester1"
 ) else (
     java -cp .;.\aima\AIMA.jar;Bicing\Bicing.jar %~1
 )
