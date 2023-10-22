fcbd8bb016693c14e4522bcd3bc274ba18f0bb3b
Jon <jon.campillo@gmail.com>
2023-10-22 17:59:18 +0200

    Tester8 final final este si

--- TesterExp8.java
+++ TesterExp8.java
@@ -53,6 +53,7 @@ public class TesterExp8
             if(i == ejecuciones-1)
             {
                 //Print datos
+                System.out.println("Heuristico primer criterio:");
                 System.out.println("Pago por bicicletas bien transladadas: " + hcBoard.beneficioTotal(false) + " euros");
                 System.out.println("Beneficio: " + hcBoard.getBeneficioReal() + " euros");
                 System.out.println("Distancia: " + hcBoard.getTotalTravelDist() + "m");
@@ -60,6 +61,52 @@ public class TesterExp8
         }
 
         double mediaTiempo = tiempoTotal/ejecuciones;
+        mediaTiempo /= 1000000.0; //Nanosegundos a milisegundos
+        System.out.println("Tiempo medio (" + ejecuciones + " ejecuciones): " + mediaTiempo + "ms");
+
+        tiempoTotal = 0.0;
+
+        for(int i = 0; i < ejecuciones; ++i)
+        {
+            Estaciones estaciones = new Estaciones(numEstaciones, numBicis, tipoDemanda, seed);
+            
+            //Heurístico usado
+            PracHeuristicFunction.Function heuristicoHC = PracHeuristicFunction.Function.Heuristico_2;
+            
+            //Tipo de solución inicial
+            PracBoard.TipoSolucion tipoSol = PracBoard.TipoSolucion.GREEDY2;
+
+            PracBoard board = new PracBoard(estaciones, maxFurgonetas);
+            board.setRedondeo(4);
+            board.creaSolucionInicial(tipoSol);
+
+            PracSuccessorFunction successorFunction = new PracSuccessorFunction(PracSuccessorFunction.SearchType.HillClimbing);
+            successorFunction.disableChange3Est();
+
+            Problem p = new Problem(board, successorFunction, new PracGoalTest(), new PracHeuristicFunction(heuristicoHC));
+
+            Search alg = new HillClimbingSearch();
+
+            double startTime = System.nanoTime();
+            SearchAgent agent = new SearchAgent(p, alg);
+            double endTime = System.nanoTime();
+
+            PracBoard hcBoard = (PracBoard)alg.getGoalState();
+
+            tiempoTotal += (endTime-startTime);
+
+            if(i == ejecuciones-1)
+            {
+                //Print datos
+                System.out.println();
+                System.out.println("Heuristico primer y segundo criterio:");
+                System.out.println("Pago por bicicletas bien transladadas: " + hcBoard.beneficioTotal(false) + " euros");
+                System.out.println("Beneficio: " + hcBoard.getBeneficioReal() + " euros");
+                System.out.println("Distancia: " + hcBoard.getTotalTravelDist() + "m");
+            }
+        }
+
+        mediaTiempo = tiempoTotal/ejecuciones;
         mediaTiempo /= 1000000.0; //Nanosegundos a milisegundos
         System.out.println("Tiempo medio (" + ejecuciones + " ejecuciones): " + mediaTiempo + "ms");
     }
