2806dd79ff248e53e73ad4efdf5b55536e89a3a9
bunnerd <sandravaro03@gmail.com>
2023-10-22 18:33:15 +0200

    archivos r

--- R/Exp2.R
+++ R/Exp2.R
@@ -4,18 +4,19 @@
 setwd("C:/Users/Sandra/Documents/Uni/IA/Lab")
 if (!require("data.table")) install.packages("data.table")
 library(data.table)
-res <- read.table("exp2Invent.txt", header = TRUE, sep = "\t")
+res2 <- read.table("exp2.txt", header = TRUE, sep = "\t")
 
-sol1 <- subset(res, set==1)
-sol2 <- subset(res, set==2)
+sol1 <- subset(res2, sol_ini=="Vacia")
+sol2 <- subset(res2, sol_ini=="Normal")
+sol3 <- subset(res2, sol_ini=="Greedy")
 
 #Comparando beneficios
-boxplot(sol1$benef, sol2$benef, names=c("sol1", "sol2"), ylab="beneficio")
+boxplot(sol1$calidad, sol2$calidad, sol3$calidad, names=c("Vacia", "Normal", "Greedy"), ylab="beneficio")
 dif12 <- sum(sol1$benef > sol2$benef)
 
 #Probabilidad de que pase esto bajo H0
 dbinom(x=dif12,size=length(sol1$benef),prob=0.5)
 
 #Comparando tiempos
-boxplot(sol1$tiempo, sol2$tiempo, names=c("sol1", "sol2"), ylab="ms")
+boxplot(sol1$tiempo, sol2$tiempo, sol3$tiempo, names=c("Vacia", "Normal", "Greedy"), ylab="ms")
 t.test(sol1$tiempo, sol2$tiempo, paired=TRUE)
