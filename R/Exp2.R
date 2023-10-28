75f083df383a1c9d40452e8a02ebbf6237a06beb
bunnerd <sandravaro03@gmail.com>
2023-10-28 20:24:12 +0200

    mñeh

--- R/Exp2.R
+++ R/Exp2.R
@@ -3,7 +3,7 @@
 setwd("C:/Users/Sandra/Documents/Uni/IA/Lab/IA-Practica-Bicing/R")
 if (!require("data.table")) install.packages("data.table")
 library(data.table)
-res2 <- read.table("exp2H2Maximo.txt", header = TRUE, sep = "\t")
+res2 <- read.table("exp2H1MEdia.txt", header = TRUE, sep = "\t")
 
 vacia <- subset(res2, sol_ini=="Vacia")
 random <- subset(res2, sol_ini=="Random")
@@ -12,7 +12,8 @@ greedy <- subset(res2, sol_ini=="Greedy")
 #Comparando beneficios
 boxplot(vacia$calidad, random$calidad, greedy$calidad, names=c("Vacia", "Aleatoria", "Greedy"), ylab="Calidad")
 difVacRand <- sum(vacia$calidad > random$calidad)
-difRandGreedy <- sum(random$calidad > greedy$calidad)
+
+difRandGreedy <- sum(greedy$calidad > random$calidad)
 
 #Probabilidad de que pase esto bajo H0
 dbinom(x=dif12,size=length(vacia$benef),prob=0.5)
@@ -22,6 +23,6 @@ boxplot(vacia$beneficio+vacia$calidad, random$beneficio+random$calidad, greedy$b
 
 #Comparando tiempos
 boxplot(vacia$tiempo, random$tiempo, greedy$tiempo, names=c("Vacía", "Aleatoria", "Greedy"), ylab="tiempo (ms)")
-t.test(vacia$tiempo, greedy$tiempo, paired=TRUE)
+t.test(vacia$tiempo, random$tiempo, paired=TRUE)
 t.test(random$tiempo, greedy$tiempo, paired=TRUE)
 
