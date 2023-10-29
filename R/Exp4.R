a237cfbf8f8baef87700a2240be78f4ad77f76b9
bunnerd <sandravaro03@gmail.com>
2023-10-29 18:17:41 +0100

    r

--- R/Exp4.R
+++ R/Exp4.R
@@ -3,7 +3,7 @@
 setwd("C:/Users/Sandra/Documents/Uni/IA/Lab/IA-Practica-Bicing/R")
 if (!require("data.table")) install.packages("data.table")
 library(data.table)
-res4 <- read.table("exp4.txt", header = TRUE, sep = "\t")
+res4 <- read.table("exp4Final.txt", header = TRUE, sep = "\t")
 
 
 meanTiempo <- aggregate(x=res4$tiempo, by=list(res4$estaciones), FUN=mean)
@@ -13,3 +13,16 @@ meanTiempo <- aggregate(x=res4$tiempo, by=list(res4$estaciones), FUN=mean)
 plot(meanTiempo$x ~ meanTiempo$Group.1, type="b", ylab="Tiempo (ms)", xlab="Estaciones", xaxt = "n")
 
 axis(1, at = c(25, 50, 75, 100, 125, 150))
+
+options(scipen = 10000)
+x <- seq(min(meanTiempo$x), max(meanTiempo$x), length.out=100)
+
+f <- function(y, x) ((x^2)/y)
+
+model <- nls(meanTiempo$Group.1 ~ f(y, meanTiempo$x), data = meanTiempo, start=list(y=10000), trace = TRUE)
+summary(model)
+
+
+f2 <- function(x) (((x^2)/coef(model)))
+lines(x,f2(x), col = "red1")
+
